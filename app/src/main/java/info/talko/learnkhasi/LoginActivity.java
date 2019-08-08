9790814bac69d3d24f5c42029e46ea46dc3189f6
Monoj Das <dasmanoj1996.md@gmail.com>
2019-08-09 00:57:41 +0530

    initial-commit

--- app/src/main/java/info/talko/learnkhasi/LoginActivity.java
+++ app/src/main/java/info/talko/learnkhasi/LoginActivity.java
@@ -2,19 +2,85 @@ package info.talko.learnkhasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import info.talko.learnkhasi.misc.Settings;
 
 public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+
         Log.d("API_KEY", Settings.getAPI_KEY());
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                Log.d("LoginActivity", account.getEmail());
+                Log.d("LoginActivity", account.getGivenName());
+                Log.d("LoginActivity", account.getFamilyName());
+                Log.d("LoginActivity", account.getDisplayName());
+                Log.d("LoginActivity", account.getId());
+                //Log.d("LoginActivity", account.getServerAuthCode());
+                Log.d("LoginActivity", account.getPhotoUrl()+"");
+
+
+
+                // Signed in successfully, show authenticated UI.
+                //updateUI(account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.d("LoginActivity", "signInResult:failed code=" + e.getStatusCode());
+                //updateUI(null);
+            }
+        }
     }
 }
